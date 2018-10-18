3e57967c6f135ba6754b49e5352949c1788ca403
tclilu <984234055@qq.com>
2018-10-18 18:31:13 +0800

    响应工具类测试

---
+++ src/test/java/com/sw/urs/util/MyResponseUtilTest.java
@@ -0,0 +1,33 @@
+package com.sw.urs.util;
+
+import com.sw.urs.model.ResponseCode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyResponseUtilTest {
+    private Logger logger = LoggerFactory.getLogger(MyResponseUtilTest.class);
+
+    /**
+     * success测试
+     */
+    @Test
+    public void success() {
+        System.out.println(MyResponseUtil.success("ok"));
+    }
+
+    /**
+     * info测试
+     */
+    @Test
+    public void info() {
+        System.out.println(MyResponseUtil.info(ResponseCode.SUCCESS,"ok"));
+    }
+}
