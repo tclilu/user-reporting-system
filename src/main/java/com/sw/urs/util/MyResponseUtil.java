372451c0e6438dda9d8cb9ce436e7cddb494f1da
tclilu <984234055@qq.com>
2018-10-18 18:28:56 +0800

    响应工具类

---
+++ src/main/java/com/sw/urs/util/MyResponseUtil.java
@@ -0,0 +1,31 @@
+package com.sw.urs.util;
+
+import com.sw.urs.model.MyResponse;
+import com.sw.urs.model.ResponseCode;
+
+/**
+ * 响应工具类
+ */
+public class MyResponseUtil {
+
+    /**
+     * 请求成功
+     * @param data 响应的数据
+     * @return
+     */
+    public static MyResponse success(Object data) {
+        return new MyResponse<>(ResponseCode.SUCCESS,data);
+    }
+
+    /**
+     * 自定义响应信息
+     * @param responseCode
+     * @param msg
+     * @return
+     */
+    public static MyResponse info(ResponseCode responseCode,String msg) {
+        MyResponse<Object> response = new MyResponse<>(responseCode);
+        response.setMsg(msg);
+        return response;
+    }
+}
