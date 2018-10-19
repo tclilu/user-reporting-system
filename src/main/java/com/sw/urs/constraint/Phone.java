8bf90971ed2541e881ca6d7f75d8f9b74c2037d3
tclilu <984234055@qq.com>
2018-10-19 23:18:57 +0800

    自定义phone注解（校验手机号格式）

---
+++ src/main/java/com/sw/urs/constraint/Phone.java
@@ -0,0 +1,22 @@
+package com.sw.urs.constraint;
+
+import com.sw.urs.validator.PhoneValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * 自定义手机号校验注解
+ */
+@Constraint(validatedBy = PhoneValidator.class)
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Phone {
+    String message() default "手机号格式不正确";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
