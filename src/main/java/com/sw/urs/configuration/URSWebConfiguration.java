7ed264546aece6c832f79f5c873ff79ae0ac3e27
tclilu <984234055@qq.com>
2018-10-20 21:35:57 +0800

    添加CORS跨域请求拦截器

--- src/main/java/com/sw/urs/configuration/URSWebConfiguration.java
+++ src/main/java/com/sw/urs/configuration/URSWebConfiguration.java
@@ -1,15 +1,20 @@
 package com.sw.urs.configuration;
 
+import com.sw.urs.interceptor.CORSInterceptor;
 import com.sw.urs.interceptor.LoginRequiredInterceptor;
 import com.sw.urs.interceptor.PassportInterceptor;
 import com.sw.urs.interceptor.PermissionInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class URSWebConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private CORSInterceptor corsInterceptor;
+
     @Autowired
     private PassportInterceptor passportInterceptor;
 
@@ -25,11 +30,26 @@ public class URSWebConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 允许跨域
+        registry.addInterceptor(corsInterceptor);
         // ticket认证
-        registry.addInterceptor(passportInterceptor);
+        registry.addInterceptor(passportInterceptor).addPathPatterns("/**");
         // 登录认证
-        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/admin/add").addPathPatterns("");
+        registry.addInterceptor(loginRequiredInterceptor).excludePathPatterns("/admin/doLogin").excludePathPatterns("/admin/logout").addPathPatterns("/admin/**");
         // 权限认证
-        registry.addInterceptor(permissionInterceptor);
+        registry.addInterceptor(permissionInterceptor).excludePathPatterns("/admin/doLogin").excludePathPatterns("/admin/logout").addPathPatterns("/admin/**");
     }
+
+//    /**
+//     * 允许跨域
+//     * @param registry
+//     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/admin/**")
+//                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedMethods("GET","POST")
+//                .maxAge(3600);
+//    }
 }
